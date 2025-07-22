a6f2d311b2f0bc7cca9679fd6219509a927b67e8
develop <bomaltennishal.@outlook.com>
2025-07-22 20:55:34 +0200

    nieuw

---
+++ 2/verdict.py
@@ -0,0 +1,62 @@
+import urllib.request/home/develop/Develop/AI
+import re
+
+url = ("https://raw.githubusercontent.com/rasbt/"
+       "LLMs-from-scratch/main/ch02/01_main-chapter-code/"
+       "the-verdict.txt")
+
+file_path = "the-verdict.txt"
+urllib.request.urlretrieve(url, file_path)
+
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+       raw_text = f.read()
+
+print("Total number of character:", len(raw_text))
+print(raw_text[:99])
+
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+print(len(preprocessed))
+print(preprocessed[:30])
+
+all_words = sorted(set(preprocessed))
+vocab_size = len(all_words)
+print(vocab_size)
+
+vocab = {token:integer for integer,token in enumerate(all_words)}
+for i, item in enumerate(vocab.items()):
+       print(item)
+       if i >= 50:
+              break
+
+class SimpleTokenizerV1:
+       def __init__(self, vocab):
+              self.str_to_int = vocab            #1
+              self.int_to_str = {i:s for s,i in vocab.items()}        #2
+
+       def encode(self, text):         #3
+              preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+              preprocessed = [
+                     item.strip() for item in preprocessed if item.strip()
+              ]
+
+              ids = [self.str_to_int[s] for s in preprocessed]
+              return ids
+
+       def decode(self, ids):         #4
+              text = " ".join([self.int_to_str[i] for i in ids])
+              text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)    #5
+              return text
+
+
+tokenizer = SimpleTokenizerV1(vocab)
+text = """"It's the last he painted, you know," 
+       Mrs. Gisburn said with pardonable pride."""
+ids = tokenizer.encode(text)
+print(ids)
+
+print(tokenizer.decode(ids))
+
+# text = "Hello, do you like tea?"
+# print(tokenizer.encode(text))
+
